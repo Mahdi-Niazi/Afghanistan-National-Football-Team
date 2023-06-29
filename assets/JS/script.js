135ec96da0d03450ea14ee9eeaebff846d30a2d3
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-06-29 10:34:52 +0430

    Fix navbar responsive Fix introduction section padding and margins Fix about page logo section Fix footer for all pages Fix main program section Fix feature players section

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,28 +1,45 @@
+const humberger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-list');
+const logo = document.querySelector('.logo');
+const bars = document.querySelector('.fa-bars');
+humberger.addEventListener('click', () => {
+  humberger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  logo.classList.toggle('active');
+  bars.classList.toggle('active');
+});
+document.querySelectorAll('.nav-items').forEach((n) => n.addEventListener('click', () => {
+  humberger.classList.remove('active');
+  navMenu.classList.remove('active');
+  logo.classList.remove('active');
+  bars.classList.remove('active');
+}));
+
 const playerDetails = [
   {
     fullName: 'Islam Amiri',
-    position: 'Plays as a centre-back',
+    position: 'Centre-back',
     description: 'Amiri made his debut in the 2005 SAFF Gold Cup against Maldives.',
     playerImage: '/assets/images/guest-player-1.png',
     altText: 'Islam Amiri',
   },
   {
     fullName: 'Faysal Shayesteh',
-    position: 'Plays as a attacking midfielder',
+    position: 'Attacking midfielder',
     description: 'Born in Kabul, Shayesteh moved to the Netherlands and played in the youth academy',
     playerImage: '/assets/images/guest-player-2.png',
     altText: 'Faysal Shayesteh',
   },
   {
     fullName: 'Abassin Alikhil',
-    position: 'Plays as a defensive midfielder',
+    position: 'Defensive midfielder',
     description: 'Abassin Alikhil is an Afghan professional footballer who plays as a defensive midfielder.',
     playerImage: '/assets/images/guest-player-3.png',
     altText: 'Abassin Alikhil',
   },
   {
     fullName: 'Mustafa Hadid',
-    position: 'Plays as a right back',
+    position: 'Right back',
     description: 'Born in Kabul, Hadid moved to Hamburg with his family in 2016 escaping the Third Afghan Civil War (1996–2001).',
     playerImage: '/assets/images/guest-player-4.png',
     altText: 'Mustafa Hadid',
@@ -46,17 +63,17 @@ window.addEventListener('load', () => {
   for (let i = 0; i < playerDetails.length; i += 1) {
     const card = `
   <div class="col-md-6">
-    <div class="row p-4">
+    <div class="row p-lg-4">
         <div class="players">
             <div class="col-md-6 align-self-start">
                 <img src="${playerDetails[i].playerImage}" alt="${playerDetails[i].altText}" class="guest-player-1">
             </div>
-            <div class="col-md-6  p-4">
+            <div class="col-md-6  p-lg-4 txt-cus">
                 <h3>${playerDetails[i].fullName}</h3>
-            <div class="border-players"></div>
                 <p class="player-position">
                     ${playerDetails[i].position}
                 </p>
+                <div class="border-players"></div>
                 <p>
                     ${playerDetails[i].description}
                 </p>
